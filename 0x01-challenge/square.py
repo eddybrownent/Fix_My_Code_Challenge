9ebc0cd0d33dcea2bfee681a2c48fb4afbd3f5e4
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-09-19 22:38:57 +0300

    fixed My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+"""
+This script has a modele Square
+"""
+
 
 class square():
-    
+    """
+    class defining a Square
+    """
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """
+        the constructor
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +24,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        returns a string rep of the square
+        """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
